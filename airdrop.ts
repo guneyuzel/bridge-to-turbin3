abfff306c9de8a739b48ce0887ca5de27b29a555
Guney Uzel <devtex16@gmail.com>
2024-07-03 21:20:21 +0300

    Fix dev-wallet.json path in .gitignore

--- airdrop.ts
+++ airdrop.ts
@@ -2,7 +2,7 @@ import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js"
 
 import wallet from "./dev-wallet.json"
 
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet)); //Import your "Solana Wallet Secret" to the dev-wallet.json to fix the error.
 const connection = new Connection("https://api.devnet.solana.com");
 
 (async () => {
